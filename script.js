fd38443ac948b9883d3a6ded77571eeb3fba86e0
segador2002 <segador200274@gmail.com>
2025-07-21 15:36:02 +0200

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function mostrarMensaje() {
-  alert('¡Bienvenido a INFORMÁTICA PALAU! Estamos aquí para ayudarte con todo lo que necesites 🖥️💬');
+  alert('¡Bienvenido a INFORMÁTICA PALAU! Estamos aquí para ayudarte con todo lo que necesites contacta sin compromiso por telefono o whatsapp en el numero 653538004 🖥️💬');
 }
